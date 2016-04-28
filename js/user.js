7597168683a82973f9c8672a6cbd8fa6f07ad3cf
s9998373 <9998373@student.swin.edu.au>
2016-04-28 18:08:12 +1000

    *Added more stub pages. *Added demo user in JS to test if AngularJS bindings are working on the front-end. *Added somewhat complete account page. *Header reflects current page. *Add change password form, backend side is incomplete.

--- js/user.js
+++ js/user.js
@@ -1,3 +1,10 @@
+/**
+ *	User class for Book Exchange
+ *	Last edit date: 28.04.2016
+ *
+ *	Purpose: Manage user information and interface it with AngularJS.
+ */
+
 var app = angular.module("bookXChange", []);
 
 app.controller("userController", function ($scope) {
@@ -5,18 +12,27 @@ app.controller("userController", function ($scope) {
 	$scope.user = {'authenticated': false};
 	
 	$scope.init = function () {
-		$scope.user.authenticated = userIsAuthenticated();
+		$scope.user.authenticated = $scope.userIsAuthenticated();
 		if ($scope.user.authenticated === true){
-			loadUserInfo();
+			$scope.loadUserInfo();
 		}
     };
 	
-	userIsAuthenticated = function(){
+	$scope.userIsAuthenticated = function(){
 		return true;
 	}
 	
-	loadUserInfo = function(){
-		$scope.user.username = "Demo";
+	$scope.loadUserInfo = function(){
+//		$scope.user.username = "Demo";
+		$scope.user.firstname = "John";
+		$scope.user.surname = "Smith";
+		$scope.user.email = "user@example.com";
+		$scope.user.telephone = "0400 000 000"
+		$scope.user.fullname = $scope.user.firstname + ' ' + $scope.user.surname;
+	}
+	
+	$scope.performLogout = function(){
+		alert("Logout stub.");
 	}
 	
 	$scope.init ();
