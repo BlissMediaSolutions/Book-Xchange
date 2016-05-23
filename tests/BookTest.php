4f4890be50378c8cad11960e67ccc3d6a8efd782
Danielle Walker <danielle@bliss.net.au>
2016-05-24 03:29:13 +1000

    Updating for Code Coverage Analysis

--- tests/BookTest.php
+++ tests/BookTest.php
@@ -4,27 +4,65 @@ include('php/book.php');
 
 class BookTest extends PHPUnit_Framework_TestCase
 {
-	//Function to run Unit Tests on the Book Class
+
+	/**
+ 	* @covers BOOK::getBookName
+ 	*/
+ 	public function testGetBookName()
+ 	{
+ 		$thisBook = new Book('Harry Potter', '1234567890', 'J. K. Rowling', 'ACME Publishing', '1');
+ 		$this->assertEquals('Harry Potter', $thisBook->getBookName());
+ 		$this->assertNotEquals('Parry Hotter', $thisBook->getBookName());
+ 	}
+
+ 	/**
+ 	* @covers BOOK::getBookISBN
+ 	*/
+ 	public function testGetBookISBN()
+ 	{
+ 		$thisBook = new Book('Harry Potter', '1234567890', 'J. K. Rowling', 'ACME Publishing', '1');
+ 		$this->assertEquals('1234567890', $thisBook->getBookISBN());
+ 		$this->assertNotEquals('1122334455', $thisBook->getBookISBN());
+ 	}
+
+ 	/**
+ 	* @covers BOOK::getBookAuthor
+ 	*/
+ 	public function testGetBookAuthor()
+ 	{
+ 		$thisBook = new Book('Harry Potter', '1234567890', 'J. K. Rowling', 'ACME Publishing', '1');
+ 		$this->assertEquals('J. K. Rowling', $thisBook->getBookAuthor());
+ 		$this->assertNotEquals('Harry Smith', $thisBook->getBookAuthor());
+ 	}
+
+	/**
+ 	* @covers BOOK::getBookPublisher
+ 	*/
+ 	public function testGetBookPublisher()
+ 	{
+ 		$thisBook = new Book('Harry Potter', '1234567890', 'J. K. Rowling', 'ACME Publishing', '1');
+ 		$this->assertEquals('ACME Publishing', $thisBook->getBookPublisher());
+ 		$this->assertNotEquals('Judge Publishing', $thisBook->getBookPublisher());
+ 	} 	
+
+ 	/**
+ 	* @covers BOOK::getBookEdition
+ 	*/
+ 	public function testGetBookEdition()
+ 	{
+ 		$thisBook = new Book('Harry Potter', '1234567890', 'J. K. Rowling', 'ACME Publishing', '1');
+ 		$this->assertEquals('1', $thisBook->getBookEdition());
+ 		$this->assertNotEquals('2', $thisBook->getBookEdition());
+ 	} 
+
 	public function testBookCreate()
 	{
 		$thisBook = new Book('Harry Potter', '1234567890', 'J. K. Rowling', 'ACME Publishing', '1');
-		$this->assertEquals('Harry Potter', $thisBook->getBookName());
-		$this->assertEquals('1234567890', $thisBook->getBookISBN());
-		$this->assertEquals('J. K. Rowling', $thisBook->getBookAuthor());
-		$this->assertEquals('ACME Publishing', $thisBook->getBookPublisher());
-		$this->assertEquals('1', $thisBook->getBookEdition());
-
 		$thisBook->setBookName('Judge Dredd');
 		$thisBook->setBookISBN('1122334455');
 		$thisBook->setBookAuthor('Harry Smith');
 		$thisBook->setBookPublisher('Judge Publishing');
 		$thisBook->setBookEdition('2');
-
-		$this->assertNotEquals('Harry Potter', $thisBook->getBookName());
-		$this->assertNotEquals('1234567890', $thisBook->getBookISBN());
-		$this->assertNotEquals('J. K. Rowling', $thisBook->getBookAuthor());
-		$this->assertNotEquals('ACME Publishing', $thisBook->getBookPublisher());
-		$this->assertNotEquals('1', $thisBook->getBookEdition());
 
 		$this->assertEquals('Judge Dredd', $thisBook->getBookName());
 		$this->assertEquals('1122334455', $thisBook->getBookISBN());
